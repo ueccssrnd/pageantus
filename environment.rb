7bb0c8324fe4c30000ab0c078ea8f51f7d611c3f
Daryll Santos <daryll.santos@gmail.com>
2013-11-23 21:24:34 +0800

    add normalize and reset lol.

--- environment.rb
+++ environment.rb
@@ -9,7 +9,7 @@ sinatra/assetpack
 
 DataMapper.setup(:default,  ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/test.rb")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+
 Dir["./helpers/*.rb"].each {|file| require file }  
 Dir["./models/*.rb"].each {|file| require file }
 
